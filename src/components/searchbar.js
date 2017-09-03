7427f96ea2b3f0bc7bf674168d26eebdd3022112
tgundavelli <tej.gundavelli@berkeley.edu>
2017-09-03 12:56:48 -0700

    finished lecture 22

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -4,10 +4,35 @@ import React, { Component } from 'react';
 //	return <input />;
 //};
 
+
+// each class based object has a state object 
+// if state of search bar is changed then the render function is reran
+
 class SearchBar extends Component{
+	// this is how we initialize state in a class based component
+	// constructor is used for initializing state 
+	// component has a constructor function and we are calling the parent constructor
+	constructor(props) {
+		super(props);
+		// we initialize state by defining a new object and passing it to this.state
+		// we want to update term to be whatever this.state is
+		// this is the only time state looks like
+		this.state = { term : ''};
+	}
+
 	render(){
-		return <input onChange = {event => console.log(event.target.value)} />;
+		// we will be changing state in the render function 
+		// always manipulate state with this.setState
+		return (
+			<div>
+			<input onChange = {event => this.setState({term : event.target.value})} />
+			</div>
+			//Value of input : {this.state.term} 
+
+			//always wrap a variable around {}
+			);
 	} // every class must have a render function
+	// this is how we put values into our search bar!!
 } 
 
-export default SearchBar
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/searchbar.js'
+import YTSearch from 'youtube-api-search'
 const API_KEY = 'AIzaSyAG3Z_VDWwBQ12gnYQqB9UXBzU3iVjKloU';
 
 // Create  a new REACT component which produces HTML
-const App = () => {
-	return (<div> 
+
+
+
+class App extends Component{
+	constructor(props) {
+		super(props);
+		this.state = {videos: []};
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) =>  {
+		this.setState({ videos });
+		});
+	}
+	render() {
+		return (<div> 
 		<SearchBar /> 
 		</div>);
+	}
+
 }
 
 
